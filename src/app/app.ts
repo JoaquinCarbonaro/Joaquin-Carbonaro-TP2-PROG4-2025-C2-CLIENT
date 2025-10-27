8465f5cdcc999dd1ad15c576ea16a4f4008afb94
JoaquinCarbonaro <joacolcarbo@gmail.com>
2025-10-27 18:43:15 -0300

    initial commit: estructura Angular

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('Joaquin-Carbonaro-TP2-PROG4-2025-C2-CLIENT');
+}
