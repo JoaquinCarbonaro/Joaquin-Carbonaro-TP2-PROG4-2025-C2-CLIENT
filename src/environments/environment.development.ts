7e7141e8ebeadcb1e3a5269c77c6a60ed5aac4ec
JoaquinCarbonaro <joacolcarbo@gmail.com>
2025-10-28 15:53:31 -0300

    add: deploy vercel

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+//entorno local de desarrollo
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://localhost:3000', //backend nest local
+  useCookies: false //false porque usamos localstorage en sprint 1
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+//entorno de produccion para vercel
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://joaquin-carbonaro-tp2-prog4-2025-c2.onrender.com', //url del backend en render
+  useCookies: false //por ahora false, luego true si usamos cookies httpOnly
+};
