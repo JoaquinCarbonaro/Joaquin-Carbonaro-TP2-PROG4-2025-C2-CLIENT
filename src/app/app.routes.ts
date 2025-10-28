cd5a22ba661da27ebec061e7ab0b136fe80cb51f
JoaquinCarbonaro <joacolcarbo@gmail.com>
2025-10-28 18:31:42 -0300

    add: fin pruebas de conexion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  // temporalmente enviamos a diagnostico para probar el back
-  { path: '', redirectTo: 'diagnostico', pathMatch: 'full' },
-  { path: 'diagnostico', loadComponent: () => import('./pages/diagnostico/diagnostico').then(m => m.Diagnostico) },
-
-  // ejemplo de tus rutas reales (dejalas descomentadas cuando las tengas)
-  //{ path: 'login', loadComponent: () => import('./pages/login/login').then(m => m.Login) },
-  //{ path: 'publicaciones', loadComponent: () => import('./pages/publicaciones/publicaciones').then(m => m.Publicaciones) },
-  //{ path: 'mi-perfil', loadComponent: () => import('./pages/mi-perfil/mi-perfil').then(m => m.MiPerfil) },
-
-  { path: '**', redirectTo: 'diagnostico' },
-];
+export const routes: Routes = [];
