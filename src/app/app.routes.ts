35a77d8ca47d1b679cc91ea1aceb798cbeccbbaa
JoaquinCarbonaro <joacolcarbo@gmail.com>
2025-11-11 20:22:14 -0300

    update: routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,30 +1,41 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { AuthGuard } from './guards/auth-guard'
+import { NoAuthGuard } from './guards/no-auth-guard'
 
 export const routes: Routes = [
 
   { //ruta raiz (por defecto)
     path: '',
     pathMatch: 'full',
-    redirectTo: 'login'
+    redirectTo: 'cargando'
+  },
+  {
+    path: 'cargando',
+    loadComponent: () => import('./pages/cargando/cargando').then((m) => m.Cargando)
   },
   {
     path: 'login',
+    canActivate: [NoAuthGuard],
     loadComponent: () => import('./pages/login/login').then((m) => m.Login)
   },
   {
     path: 'registro',
+    canActivate: [NoAuthGuard],
     loadComponent: () => import('./pages/registro/registro').then((m) => m.Registro)
   },
   {
     path: 'publicaciones',
+    canActivate: [AuthGuard],
     loadComponent: () => import('./pages/publicaciones/publicaciones').then((m) => m.Publicaciones)
   },
   {
     path: 'publicaciones/:id',
+    canActivate: [AuthGuard],
     loadComponent: () => import('./pages/publicacion/publicacion').then((m) => m.PublicacionDetalle)
   },
   {
     path: 'mi-perfil',
+    canActivate: [AuthGuard],
     loadComponent: () => import('./pages/mi-perfil/mi-perfil').then((m) => m.MiPerfil)
   },
   {
@@ -35,4 +46,4 @@ export const routes: Routes = [
     path: '**',
     redirectTo: 'error'
   }
-];
+]
