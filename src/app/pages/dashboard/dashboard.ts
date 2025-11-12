11d017dcb7ffe273f92f10f35cfb75841ba58253
JoaquinCarbonaro <joacolcarbo@gmail.com>
2025-11-12 20:11:22 -0300

    add: logica page dashboard

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router'
 
 @Component({
+  standalone: true,
   selector: 'app-dashboard',
-  imports: [],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
+  imports: [RouterLink, RouterLinkActive, RouterOutlet]
 })
 export class Dashboard {
-
+  //defino las opciones visibles en el panel
+  protected readonly opciones = [
+    { path: 'usuarios', texto: 'Usuarios', icono: 'fas fa-users' }
+  ]
 }
